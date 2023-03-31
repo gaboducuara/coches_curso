c7df9d7ccf6b45e100ced6b29379a636fe99f58c
Gabriel Mancilla Ducuara <Gabrielmancilladucuaraxgmail.com>
2023-03-31 18:45:59 -0500

    Primeros pasos del curso desarrollo web intermedio

---
+++ src/main/java/com/proyecto/coches/projec_coches/ProjecCochesApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.coches.projec_coches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecCochesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecCochesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/coches/projec_coches/ProjecCochesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.coches.projec_coches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecCochesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
